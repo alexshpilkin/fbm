b36276fb282f0eaf3ffb46265bf7a703a8e430c7
Alexander Shpilkin <ashpilkin@gmail.com>
2019-12-13 16:47:10 +0300

    Introduce options for comparison plots

--- compare.py
+++ compare.py
@@ -13,9 +13,8 @@ def readheaders(file):
 		headers[name.strip()] = value.strip()
 	return headers
 
-EPSILON = 1e-6
-def close(x, y):
-	return abs(x - y) <= EPSILON * min(abs(x), abs(y))
+def close(x, y, epsilon):
+	return abs(x - y) <= epsilon * min(abs(x), abs(y))
 
 def filtervalues(it):
 	return filter(lambda line: not line.startswith('#'), it)
@@ -23,7 +22,7 @@ def filtervalues(it):
 def floats(line):
 	return [float(x) for x in line.split()]
 
-def readvalues(file1, file2):
+def readvalues(file1, file2, epsilon):
 	h1, h2 = readheaders(file1), readheaders(file2)
 	assert int(h1['Iterations']) == int(h2['Iterations'])
 	logn = int(h1['Log of grid size'])
@@ -32,7 +31,7 @@ def readvalues(file1, file2):
 	points, errors = 0, 0
 	for line1, line2 in zip(filtervalues(file1), filtervalues(file2)):
 		points += 1
-		if not all(close(x1, x2)
+		if not all(close(x1, x2, epsilon)
 		           for x1, x2 in zip(floats(line1), floats(line2))):
 			errors += 1
 
@@ -40,16 +39,23 @@ def readvalues(file1, file2):
 	return (logn + levels, (errors + 0.5)/(points + 1.0),
 	        np.sqrt(errors*(points - errors)/points + 0.25)/(points + 1))
 
-def plotvalues(file1, file2):
-	x, y, yerr = readvalues(file1, file2)
-	plt.errorbar(x, y, yerr, fmt='.k')
+def plotvalues(file1, file2, epsilon, fmt='.k'):
+	x, y, yerr = readvalues(file1, file2, epsilon)
+	plt.errorbar(x, y, yerr, fmt=fmt)
 
 if __name__ == '__main__':
 	from sys import argv
 
+	epsilon, fmt = 1e-6, '.k'
 	for name1, name2 in zip(argv[1::2], argv[2::2]):
+		if name1 == '-e':
+			epsilon = float(name2)
+			continue
+		if name1 == '-f':
+			fmt = name2
+			continue
 		with open(name1, 'r') as file1, open(name2, 'r') as file2:
-			plotvalues(file1, file2)
+			plotvalues(file1, file2, epsilon, fmt=fmt)
 	plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
 	plt.xlabel("$L$")
 	plt.ylabel("Error probability")
